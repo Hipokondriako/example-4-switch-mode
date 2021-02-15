1ed814f7cfec22f37b7c2a6fb6b90e8213a95c5d
Hipokondriako <79016081+Hipokondriako@users.noreply.github.com>
2021-02-15 16:56:12 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,11 +6,31 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     Count += 1
 })
+function Green () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P2, 0)
+}
+function White () {
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P2, 1023)
+}
+function Blue () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 1023)
+}
+function Red () {
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+}
 // "Count" keps track on how many times we pressed A and B
-let Count = 0
+let Count = 1000
 let Mode = 0
 basic.forever(function () {
-    // "Mode" indicates how the LED is going to behave.  It´s the remainder of "count"/4, as an absolute number so it´s always positive.
+    // "Mode" indicates how the LED is going to behave.  It´s the remainder of "Count"/4, as an absolute number so it´s always positive.
     // Count 0, Mode 0
     // Count 1, Mode 1
     // ...
@@ -20,25 +40,17 @@ basic.forever(function () {
     // Count 15, Mode 3
     // 
     Mode = Math.abs(Count % 4)
+    // Mode 0, White light
     // Mode 1, Red light
     // Mode 2, Green light
     // Mode 3, Blue light
-    // Every other mode (only 0), white light
-    if (Mode == 1) {
-        pins.analogWritePin(AnalogPin.P0, 1023)
-        pins.analogWritePin(AnalogPin.P1, 0)
-        pins.analogWritePin(AnalogPin.P2, 0)
+    if (Mode == 0) {
+        White()
+    } else if (Mode == 1) {
+        Red()
     } else if (Mode == 2) {
-        pins.analogWritePin(AnalogPin.P0, 0)
-        pins.analogWritePin(AnalogPin.P1, 1023)
-        pins.analogWritePin(AnalogPin.P2, 0)
+        Green()
     } else if (Mode == 3) {
-        pins.analogWritePin(AnalogPin.P0, 0)
-        pins.analogWritePin(AnalogPin.P1, 0)
-        pins.analogWritePin(AnalogPin.P2, 1023)
-    } else {
-        pins.analogWritePin(AnalogPin.P0, 1023)
-        pins.analogWritePin(AnalogPin.P1, 1023)
-        pins.analogWritePin(AnalogPin.P2, 1023)
+        Blue()
     }
 })
