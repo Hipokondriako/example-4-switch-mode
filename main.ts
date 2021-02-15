d395f540c100f16cc1a96300ff9d4b22e1b69a90
Hipokondriako <79016081+Hipokondriako@users.noreply.github.com>
2021-02-15 11:36:17 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,29 @@
+// "Count" decreases by 1 when A is pressed
 input.onButtonPressed(Button.A, function () {
     Count += -1
 })
+// "Count" increases by 1 when B is pressed
 input.onButtonPressed(Button.B, function () {
     Count += 1
 })
+// "Count" keps track on how many times we pressed A and B
 let Count = 0
 let Mode = 0
 basic.forever(function () {
+    // "Mode" indicates how he LED is ging to behave.   It´s the remainder of "count"/4, as an absolute number so it´s alwats positive.
+    // Count 0, Mode 0
+    // Count 1, Mode 1
+    // ...
+    // Count 4, Mode 0
+    // Count 5, Mode 1
+    // ....
+    // Count 15, Mode 3
+    // 
     Mode = Math.abs(Count % 4)
+    // Mode 1, Red light
+    // Mode 2, Green light
+    // Mode 3, Blue light
+    // Every other mode (only 0), white light
     if (Mode == 1) {
         pins.analogWritePin(AnalogPin.P0, 1023)
         pins.analogWritePin(AnalogPin.P1, 0)
